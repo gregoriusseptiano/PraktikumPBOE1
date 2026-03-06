56b65ef1cde2d132d3800f4d2dec44ff207d5abc
gregoriusseptiano <gregoriusariadi@gmail.com>
2026-03-06 19:20:40 +0700

    Praktikum Pertemuan 3

--- Pertemuan3/src/MMahasiswa.java
+++ Pertemuan3/src/MMahasiswa.java
@@ -5,37 +5,18 @@
  */
 
 public class MMahasiswa {
-
     public static void main(String[] args) {
-
-        // Membuat objek MataKuliah
         MataKuliah PBO = new MataKuliah("PBO", "Pemrograman Berorientasi Objek", 3);
         MataKuliah MBD = new MataKuliah("MBD", "Manajemen Basis Data", 3);
-
-        // Membuat objek Mahasiswa
         Mahasiswa M1 = new Mahasiswa("234", "Citra", "Informatika");
-
-        // Membuat objek Dosen
         Dosen D1 = new Dosen("123", "Andi", "Informatika");
-
-        // Membuat objek Kendaraan
         Kendaraan K1 = new Kendaraan("H1234AB", "motor");
-
-        // Relasi objek
         M1.setDosenWali(D1);
         M1.setKendaraan(K1);
-
-        // Menambahkan mata kuliah
         M1.addMatKul(PBO);
         M1.addMatKul(MBD);
-
-        // Menampilkan detail mahasiswa
         M1.printDetailMhs();
-
-        // Menampilkan jumlah mata kuliah
         System.out.println("Jumlah Mata Kuliah = " + M1.getJumlahMatKul());
-
-        // Menampilkan jumlah SKS
         System.out.println("Jumlah SKS Mata Kuliah = " + M1.getJumlahSKS());
     }
 }
