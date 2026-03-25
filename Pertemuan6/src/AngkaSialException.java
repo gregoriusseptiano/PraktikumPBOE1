bde02e5826a7f5afa3d9db500ff1e57d90659efa
gregoriusseptiano <gregoriusariadi@gmail.com>
2026-03-25 21:05:28 +0700

    Praktikum PBO Pertemuan 6

---
+++ Pertemuan6/src/AngkaSialException.java
@@ -0,0 +1,11 @@
+/* Nama File    : AngkaSialException.java
+ * Deskripsi    : Eksepsi buatan sendiri, menolak masukan angka 13!
+ * Pembuat      : Gregorius Septiano Ariadi / 24060124120026
+ * Tanggal      : Kamis, 26 Maret 2026
+ */
+
+public class AngkaSialException extends Exception {
+    public AngkaSialException() {
+        super("jangan memasukkan angka 13 karena angka sial !!!");
+    }
+}
